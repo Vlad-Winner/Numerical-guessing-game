5432221308ceec61d06450278395428b4c7da9a4
Vlad-Winner <vlad-win@ya.ru>
2022-10-26 09:31:38 +0700

    Добавлена возможность перезапуска игры - Изменены ответы в случае победы - добавлена возможность играть дальше без нового запуска игры

--- Numerical guessing game.py
+++ Numerical guessing game.py
@@ -1,5 +1,5 @@
+from ast import Break
 import random
-n = random.randint(1, 100)
 print('Добро пожаловать в числовую угадайку')
 
 def is_valid(text):
@@ -8,26 +8,34 @@ def is_valid(text):
     else:
         return False
 
-counter = 0
+game = True
+while game:
+    counter = 0
+    n = random.randint(1, 100)
 
-while True:
-    x = input('Введите целое число от 1 до 100 включительно: ')
-    if not is_valid(x):
-        print('А может быть все-таки введем целое число от 1 до 100?')
-        continue
-    x = int(x)
-    counter += 1
-    if x > n:
-        print('Ваше число больше загаданного, попробуйте еще разок')
-    elif x < n:
-        print('Ваше число меньше загаданного, попробуйте еще разок')
-    else:
-        print('Вы угадали, поздравляем!')
-        if counter == 1:
-            print('Вы угадали с первого раза!')
-        elif counter < 8:
-            print(f'Вы угадали с {counter} попыток')
+    while True:
+        x = input('Введите целое число от 1 до 100 включительно: ')
+        if not is_valid(x):
+            print('А может быть все-таки введем целое число от 1 до 100?')
+            print()
+            continue
+        x = int(x)
+        counter += 1
+        if x > n:
+            print('Ваше число больше загаданного, попробуйте еще разок')
+        elif x < n:
+            print('Ваше число меньше загаданного, попробуйте еще разок')
         else:
-            print(f'Вы угадали с {counter}-ой попытки')
-        print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+            if counter == 1:
+                print('Вы угадали с первого раза! Поздравляем!')
+            elif counter < 8:
+                print(f'Вы угадали с {counter} попыток, поздравляем!')
+            else:
+                print(f'Вы угадали с {counter}-ой попытки, поздравляем!')
+            break
+        print()
+    print()
+    if not input('Сиграем еще? Ответьте да или нет: ').lower() == 'да':
         break
+print()
+print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
