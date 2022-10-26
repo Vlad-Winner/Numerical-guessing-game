005d01ad619a7477af29edec07036143f016a7db
Vlad-Winner <vlad-win@ya.ru>
2022-10-26 09:01:03 +0700

    Добавлен счетчик попыток, с выводом игроку.

--- Numerical guessing game.py
+++ Numerical guessing game.py
@@ -8,17 +8,26 @@ def is_valid(text):
     else:
         return False
 
+counter = 0
+
 while True:
     x = input('Введите целое число от 1 до 100 включительно: ')
     if not is_valid(x):
         print('А может быть все-таки введем целое число от 1 до 100?')
         continue
     x = int(x)
+    counter += 1
     if x > n:
         print('Ваше число больше загаданного, попробуйте еще разок')
     elif x < n:
         print('Ваше число меньше загаданного, попробуйте еще разок')
     else:
         print('Вы угадали, поздравляем!')
+        if counter == 1:
+            print('Вы угадали с первого раза!')
+        elif counter < 8:
+            print(f'Вы угадали с {counter} попыток')
+        else:
+            print(f'Вы угадали с {counter}-ой попытки')
         print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
         break
